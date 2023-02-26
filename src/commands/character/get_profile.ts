d2ea5d5318801ef94d31b19fe85284e6ded1a7af
ViniciusIth <vini.ithalo123@gmail.com>
2023-02-25 23:17:56 -0300

    `getProfile` now handles null IDs

--- src/commands/character/get_profile.ts
+++ src/commands/character/get_profile.ts
@@ -12,17 +12,22 @@ const getProfile: ICommandInteraction = {
     .setName('get_profile')
     .setDescription('get the character by id')
     .addStringOption((option) =>
-      option
-        .setName('character_id')
-        .setDescription('The chosen character')
-        .setRequired(true)
+      option.setName('character_id').setDescription('The chosen character')
     ),
   async execute(interaction: CommandInteraction): Promise<void> {
     const characterId = interaction.options.get('character_id')?.value;
+    let character = null;
 
-    const character = await Character.findById(characterId).exec();
+    if (characterId) {
+      character = await Character.findById(characterId).exec();
+    } else {
+      character = await Character.findOne({
+        userId: interaction.user.id,
+      }).exec();
+    }
 
     if (!character) {
+      interaction.reply({ content: 'Character not found', ephemeral: true });
       return;
     }
 
